944672d8e1deb9c453bc7fdb3f710e801555d9da
duqicauc <nkduqi@126.com>
2023-07-20 11:57:00 +0800

    自己机器上执行验证

--- liteflow-sqldb-demo/src/main/resources/db.sql
+++ liteflow-sqldb-demo/src/main/resources/db.sql
@@ -1,3 +1,8 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`liteflow_rule_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `liteflow_rule_db`;
+
+DROP TABLE IF EXISTS `chain`;
 create table chain
 (
     id               bigint auto_increment
@@ -9,7 +14,7 @@ create table chain
     create_time      datetime    null
 );
 
-
+DROP TABLE IF EXISTS `script`;
 create table script
 (
     id               bigint auto_increment
@@ -21,9 +26,9 @@ create table script
     script_type      varchar(16) null
 );
 
-INSERT INTO poseidon.chain (id, application_name, chain_name, chain_desc, el_data, create_time) VALUES (1, 'demo', 'chain1', '测试流程', 'THEN(a, b, c, s1);', '2022-09-19 19:31:00');
+INSERT INTO liteflow_rule_db.chain (id, application_name, chain_name, chain_desc, el_data, create_time) VALUES (1, 'demo', 'chain1', '测试流程', 'THEN(a, b, c, s1);', '2022-09-19 19:31:00');
 
-INSERT INTO poseidon.script (id, application_name, script_id, script_name, script_data, script_type) VALUES (1, 'demo', 's1', '脚本s1', 'import cn.hutool.core.date.DateUtil
+INSERT INTO liteflow_rule_db.script (id, application_name, script_id, script_name, script_data, script_type) VALUES (1, 'demo', 's1', '脚本s1', 'import cn.hutool.core.date.DateUtil
 
 def date = DateUtil.parse("2022-10-17 13:31:43")
 println(date)
@@ -42,5 +47,5 @@ defaultContext.setData("student",student)
 def a=3
 def b=2
 defaultContext.setData("s1",a*b)', 'script');
-INSERT INTO poseidon.script (id, application_name, script_id, script_name, script_data, script_type) VALUES (2, 'demo', 's2', '脚本s2', 'defaultContext.setData("s2","hello")', 'script');
+INSERT INTO liteflow_rule_db.script (id, application_name, script_id, script_name, script_data, script_type) VALUES (2, 'demo', 's2', '脚本s2', 'defaultContext.setData("s2","hello")', 'script');
 
